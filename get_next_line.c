b21de580556a4cf3bb7f163790ba7505d1d29158
Zayed <zayed.ahmed123321@gmail.com>
2023-09-06 22:24:29 +0400

    buffering

--- get_next_line.c
+++ get_next_line.c
@@ -4,42 +4,20 @@ char *get_next_line(int fd)
 {
     static char *my_buffer;
     char *result;
-    int n;
 
-    n = 0;
-    my_buffer = NULL;
     if (!(result = malloc(BUFFER_SIZE + 1)))
         return (NULL);
-    while(read(fd, &result[n], 1) > 0)
-    {
-        if (result[n] == '\n')
-        {
-            result[++n] = '\0';
-            my_buffer = &result[n + 1];
-            return (result);
-        }
-        n++;
-    }
-    if (n == 0)
-    {
-        free (result);
-        return (NULL);
-    }
-    result[n] == '\0';
+
     return (result);
 }
 
 int main()
 {
     int fd = open("h.txt", O_RDONLY);
-    char *line;
-    
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
+    char *str;
 
-    close(fd);
-    return (0);
+    str = malloc(BUFFER_SIZE + 1);
+    read(fd, str, BUFFER_SIZE);
+    printf("%s", str);
 }
+
--- get_next_line.h
+++ get_next_line.h
@@ -1,11 +1,13 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 1
+#endif
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
-
-#define BUFFER_SIZE 1
 
 #endif
