d0b4d4298b91f569f9cd4bf3ed1edf106c975476
Zayed <zayed.ahmed123321@gmail.com>
2023-09-07 11:16:36 +0400

    copy file

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,23 @@
 #include "get_next_line.h"
 
+char *copy_file(int fd)
+{
+    char *str;
+    char *res;
+    int n;
+
+    n = 0;
+    res = malloc(BUFFER_SIZE + 1);
+    str = malloc(BUFFER_SIZE + 1);
+    while(read(fd, &str[n], 1) > 0)
+    {
+        res[n] = str[n];
+        printf("%c\n", str[n++]);
+    }
+    res[n] = '\0';
+    return (res);
+}
+
 char *get_next_line(int fd)
 {
     static char *my_buffer;
@@ -7,6 +25,7 @@ char *get_next_line(int fd)
 
     if (!(result = malloc(BUFFER_SIZE + 1)))
         return (NULL);
+    
 
     return (result);
 }
@@ -15,9 +34,8 @@ int main()
 {
     int fd = open("h.txt", O_RDONLY);
     char *str;
-
     str = malloc(BUFFER_SIZE + 1);
-    read(fd, str, BUFFER_SIZE);
-    printf("%s", str);
+    str = copy_file(fd);
+    printf("%s\n", str);
 }
 
